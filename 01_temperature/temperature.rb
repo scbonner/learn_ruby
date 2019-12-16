7be412449a2e02955c6968d5907112713ec34b4c
scbonner <codingbootcamp918@gmail.com>
2019-12-16 06:17:01 +0000

    answers to temperature test

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,24 @@
 #write your code here
+#write your code here
+def ftoc(temperature)
+  temperature = (temperature - 32.0) * (5.0/9.0)
+end
+def ctof(temperature)
+  temperature = (temperature * (9.0 / 5.0)) + 32
+end
+
+temperature conversion functions
+  #ftoc
+    converts freezing temperature
+    converts boiling temperature
+    converts body temperature
+    converts arbitrary temperature
+  #ctof
+    converts freezing temperature
+    converts boiling temperature
+    converts arbitrary temperature
+    converts body temperature
+
+Finished in 0.00831 seconds (files took 0.10017 seconds to load)
+8 examples, 0 failures
+
